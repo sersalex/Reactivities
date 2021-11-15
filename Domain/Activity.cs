44d6a3613489529e7f951573f4f16cfa4b9a24a5
Alekseev Sergey <sersalex@ozon.ru>
2021-11-15 17:02:22 +0300

    add files

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,21 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
     public class Activity
     {
+       [Required]
         public Guid Id { get; set; }
         
         public string Title { get; set; }
-
+        [Required]
         public DateTime Date { get; set; }
-
+      
         public string Description { get; set; }
-
+       
         public string Category { get; set; }
-        
+      
         public string City { get; set; }
-        
+  
         public string Venue { get; set; }
     }
 }   
